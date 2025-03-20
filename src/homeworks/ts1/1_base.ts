a52ddd715930b3d3cf931bc61fc506116891f80d
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-03-21 08:35:11 +1000

    fix(1_base.ts): define new types

--- src/homeworks/ts1/1_base.ts
+++ src/homeworks/ts1/1_base.ts
@@ -18,7 +18,12 @@ export const round = (value: number, accuracy = 2): number => {
 const transformRegexp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString: string): { x: number; y: number } => {
+type Point = {
+  x: number;
+  y: number;
+};
+
+export const getTransformFromCss = (transformCssString: string): Point => {
   const data = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
@@ -27,7 +32,13 @@ export const getTransformFromCss = (transformCssString: string): { x: number; y:
   };
 };
 
-export const getColorContrastValue = ([red, green, blue]: [number, number, number]): number =>
+type RGB = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
+export const getColorContrastValue = ({ red, green, blue }: RGB): number =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
@@ -36,28 +47,32 @@ export const getContrastType = (contrastValue: number): 'black' | 'white' => (co
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color: string): void => {
+export const checkColor = (color: string): void | never => {
   if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
 };
 
-export const hex2rgb = (color: string): [red: number, green: number, blue: number] => {
+export const hex2rgb = (color: string): RGB => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
     const green = parseInt(color.substring(2, 3), 16);
     const blue = parseInt(color.substring(3, 4), 16);
-    return [red, green, blue];
+    return { red, green, blue };
   }
   const red = parseInt(color.substring(1, 3), 16);
   const green = parseInt(color.substring(3, 5), 16);
   const blue = parseInt(color.substring(5, 8), 16);
-  return [red, green, blue];
+  return { red, green, blue };
 };
 
-export const getNumberedArray = <T>(arr: T[]): Array<{ value: T; number: number }> =>
-  arr.map((value, number) => ({ value, number }));
+type NumberedValue<T> = {
+  value: T;
+  number: number;
+};
 
-export const toStringArray = <T>(arr: Array<{ value: T; number: number }>): string[] =>
+export const getNumberedArray = <T>(arr: T[]): NumberedValue<T>[] => arr.map((value, number) => ({ value, number }));
+
+export const toStringArray = <T>(arr: NumberedValue<T>[]): string[] =>
   arr.map(({ value, number }) => `${value}_${number}`);
 
 export interface Customer {
