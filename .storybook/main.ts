10f2dc0f8da16a74a6561cd0b0e0c845f590c119
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-03-24 15:19:08 +1000

    refactor: wrap existing stories into dedicated folders
    - Reorganized story files into individual folders within `src/stories`
    - Ensured all stories and documentation files are correctly nested in their respective directories
    
    BREAKING CHANGE: Story paths have been updated; ensure imports and references are adjusted accordingly.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx", 
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
