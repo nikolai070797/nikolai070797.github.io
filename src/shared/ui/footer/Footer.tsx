711c0e3b7a0b5e1123aed06882191f0150e8f26a
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-05-06 09:26:30 +1000

    feat: add toggleLocalization story

--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -1,11 +1,12 @@
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-
-    return (
-        <>
-            <h1>Footer</h1>
-        </>
-    );
-}
+  const { i18n, t } = useTranslation('translation', { keyPrefix: 'components.CartButtonAdd' });
+  return (
+    <>
+      <h1>Footer {i18n.language.toUpperCase()}</h1>
+    </>
+  );
+};
 
 export default Footer;
