320492301861659d89f1cc205b8a0b3c8549e67e
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-05-06 08:49:47 +1000

    fix: add translate title for cart page

--- src/pages/cart/index.ts
+++ src/pages/cart/index.ts
@@ -1,7 +1,9 @@
+import { useTranslation } from 'react-i18next';
 import CartPage from './ui/Cart';
 
 export const meta = () => {
-  return [{ title: 'Корзина' }];
+  const { t } = useTranslation('translation', { keyPrefix: 'pages.cart' });
+  return [{ title: t("title") }];
 };
 
 export default CartPage;
