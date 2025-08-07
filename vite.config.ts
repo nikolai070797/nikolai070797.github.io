ca35b38c726f8958a3aafcdd67558ff710bcc5d3
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-08-07 11:24:01 +1000

    revert: restore vite.config.ts setup for HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,45 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
+import fs from 'fs';
+import { env } from 'process';
+import path, { resolve } from 'path';
+import child_process from 'child_process';
+
+const baseFolder =
+  env.APPDATA !== undefined && env.APPDATA !== '' ? `${env.APPDATA}/ASP.NET/https` : `${env.HOME}/.aspnet/https`;
+
+const certificateName = 'OTUS.client';
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+if (!fs.existsSync(baseFolder)) {
+  fs.mkdirSync(baseFolder, { recursive: true });
+}
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  if (
+    0 !==
+    child_process.spawnSync(
+      'dotnet',
+      ['dev-certs', 'https', '--export-path', certFilePath, '--format', 'Pem', '--no-password'],
+      { stdio: 'inherit' }
+    ).status
+  ) {
+    throw new Error('Could not create certificate.');
+  }
+}
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    open: false,
+    port: 5173,
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
+    },
+  },
   resolve: {
     alias: {
       '@locales': resolve(__dirname, './src/app/localization/locales'),
@@ -13,13 +48,10 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      // input: {
-      //   main: resolve(__dirname, 'index.html'),
-      // },
       output: {
         assetFileNames: (assetInfo) => {
-          const { names } = assetInfo;
-          if (names.includes('locales')) {
+          const { name } = assetInfo;
+          if (name?.endsWith('.json') && name.includes('locales')) {
             return 'locales/[name][extname]';
           }
           // Добавляем обработку других файлов
