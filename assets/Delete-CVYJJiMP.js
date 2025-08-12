3592667e81b1d05d2ff566f67a207948b642c3e9
Николай Тартышный <kolyatartyshnyi@mail.ru>
2025-08-12 10:40:20 +1000

    Updates

---
+++ assets/Delete-CVYJJiMP.js
@@ -0,0 +1,1 @@
+import{p as h}from"./chunk-C37GKA54-1aOc6qnp.js";import{q as o}from"./Typography-DyWZUj9a.js";const c=o(h.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"})),a=o(h.jsx("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}));export{c as A,a as D};
---
+++ assets/useThemeProps-WBOW5CEa.js
@@ -0,0 +1,1 @@
+import{R as r,U as p,F as m}from"./Typography-DyWZUj9a.js";const c=r();function u(n){const{theme:e,name:o,props:s}=n;return!e||!e.components||!e.components[o]||!e.components[o].defaultProps?s:p(e.components[o].defaultProps,s)}function f({props:n,name:e,defaultTheme:o,themeId:s}){let t=m(o);return s&&(t=t[s]||t),u({theme:t,name:e,props:n})}export{c as s,f as u};
